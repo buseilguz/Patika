442692638e649ad65cec4ae3a4cd5bf07a432d1c
buseilguz <buse.ilguz@gmail.com>
2022-11-10 20:14:21 +0300

    Manav Kasa Programı

--- NotOrtalaması/src/ManavKasa.java
+++ NotOrtalaması/src/ManavKasa.java
@@ -6,7 +6,7 @@ public class ManavKasa {
 		
 		double pear,apple,tomato,banana,eggplant;
 		double total;
-
+		//istenilen deðiþkenlere kullanýcýdan alýnan deðerler atanýyor.
 		System.out.println("Armut kaç kilo ? :");
 		pear=scn.nextDouble();
 		System.out.println("Elma kaç kilo ? :");
@@ -15,9 +15,10 @@ public class ManavKasa {
 		tomato=scn.nextDouble();
 		System.out.println("Muz kaç kilo ? :");
 		banana=scn.nextDouble();
+	
 		System.out.println("Patlýcan kaç kilo ? :");
 		eggplant=scn.nextDouble();
-		
+		//ödenecek toplam tutar hesaplanýyor.
 		total=(pear*2.14)+(apple*3.67)+
 				(tomato*1.11)+(banana*0.95)+(eggplant*5);
 		System.out.println("Toplam ödenecek tutar :"+total+"TL");
